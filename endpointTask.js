b0cb0d1b9811c8be79ee78d52bfedd81e05b74b2
Allenkayode <64140701+Allenkayode@users.noreply.github.com>
2023-09-11 20:58:00 +0100

    Update endpointTask.js

--- endpointTask.js
+++ endpointTask.js
@@ -37,7 +37,7 @@ const endpoint_task1 = (req, res) => {
       github_repo_url: github_url ,
       status_code: 200,
     };
-    return res.json({response });
+    return res.json({respons.body });
   } catch (error) {
     let message = error.message;
     return console.log(message);
