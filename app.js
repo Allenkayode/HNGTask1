7022c6ec07dd5029981a06564a543d03e71fb55c
Allenkayode <64140701+Allenkayode@users.noreply.github.com>
2023-09-12 13:24:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 let port = 3000;
 
 // this route takes two queries
-app.get("/hng_api", endpoint_task1);
+app.get("/api", endpoint_task1);
 
 app.listen(port, () => {
   console.log(`server is runnning on ${port}`);
